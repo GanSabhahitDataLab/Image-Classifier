9e2e487fae6b29a265939cf9d5fccbdc12ffa7cd
Ganapati Sabhahit <54753489+GanSabhahitDataLab@users.noreply.github.com>
2020-04-21 21:58:40 -0700

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,133 +1,91 @@
 import matplotlib.pyplot as plt
-import argparse
-from time import time, sleep
-import json
 import numpy as np
 import torch
-from torchvision import datasets, transforms, models
-from torch import nn, optim
-from PIL import Image
+from torch import nn
+from torch import tensor
+from torch import optim
 import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+import torchvision.models as models
+from collections import OrderedDict
+import json
+import PIL
+from PIL import Image
+import argparse
 
-from model import load_checkpoint
-from utils import process_image
-from utils import show_prediction
+import utils
 
-# Main program function defined below
-def main():
-    # Measures total program runtime by collecting start time
-    start_time = time()
-    
-    # Creates & retrieves Command Line Arugments
-    in_arg = get_input_args()
-        
-    # Set device to cuda if gpu flag is set
-    device = 'cuda' if in_arg.gpu==True else 'cpu'
-    
-    # If given, read the mapping of categories to class names
-    cat_to_name = {}
-    if in_arg.category_names:
-        with open(in_arg.category_names, 'r') as f:
-            cat_to_name = json.load(f)
-    
-    # Load checkpoint
-    model, _, _ = load_checkpoint(in_arg.checkpoint)
+#Command Line Arguments
 
-    # Predict classes
-    probs, classes = predict(in_arg.img_path, model, device, in_arg.top_k)
-    
-    # Convert categories to real names if a mapping was given
-    if cat_to_name:
-        classes = [cat_to_name[str(cat)] for cat in classes]
+ap = argparse.ArgumentParser(
+    description='predict-file')
+ap.add_argument('input_img', default='./flowers/test/1/image_06752.jpg', nargs='*', action="store", type = str)
+ap.add_argument('checkpoint', default='./mycheckpoint.pth', nargs='*', action="store",type = str)
+ap.add_argument('--top_k', default=5, dest="top_k", action="store", type=int)
+ap.add_argument('--category_names', dest="category_names", action="store", default='cat_to_name.json')
+ap.add_argument('--gpu', default="gpu", action="store", dest="gpu")
 
-    # Print results
-    print('\nThe top {} most likely classes are:'.format(in_arg.top_k))
-    max_name_len = len(max(classes, key=len))
-    row_format ="{:<" + str(max_name_len + 2) + "}{:<.4f}"
-    for prob, name in zip(probs, classes):
-        print(row_format.format(name, prob))
-    
-    # Measure total program runtime by collecting end time
-    end_time = time()
-    
-    # Computes overall runtime in seconds & prints it in hh:mm:ss format
-    tot_time = end_time - start_time
-    print("\n** Total Elapsed Runtime:",
-          str(int((tot_time/3600)))+":"+str(int((tot_time%3600)/60))+":"
-          +str(int((tot_time%3600)%60)) )
-    
+pa = ap.parse_args()
+path_image = pa.input_img
+number_of_outputs = pa.top_k
+power = pa.gpu
+input_img = pa.input_img
+path = pa.checkpoint
+
+data_dir = "flowers"
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+test_dir = data_dir + '/test'
+
+  
+
+train_transforms = transforms.Compose([transforms.RandomRotation(50),
+                                           transforms.RandomResizedCrop(224),
+                                           transforms.RandomHorizontalFlip(),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize([0.485, 0.456, 0.406],
+                                                                [0.229, 0.224, 0.225])])
 
 
-# Functions defined below
-def get_input_args():
-    """
-    Retrieves and parses the command line arguments created and defined using
-    the argparse module. This function returns these arguments as an
-    ArgumentParser object.
-    Parameters:
-     None - simply using argparse module to create & store command line arguments
-    Returns:
-     parse_args() -data structure that stores the command line arguments object  
-    """
-    # Create parser 
-    parser = argparse.ArgumentParser()
+test_transforms = transforms.Compose([transforms.Resize(256),
+                                          transforms.CenterCrop(224),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize([0.485, 0.456, 0.406],
+                                                               [0.229, 0.224, 0.225])])
 
-    # Add command line arguments
-    parser.add_argument('img_path', type=str,
-                        help='path to input image')
-    parser.add_argument('checkpoint', type=str, 
-                        help='path to a saved checkpoint')
-    parser.add_argument('--top_k', type=int, default=3, 
-                        help='return top K most likely classes')
-    parser.add_argument('--category_names', type=str,
-                        help='path to a JSON file that maps the class values to other category names')
-    parser.add_argument('--gpu', action='store_true', 
-                        help='boolean switch: use GPU for inference')
+validation_transforms = transforms.Compose([transforms.Resize(256),
+                                                transforms.CenterCrop(224),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize([0.485, 0.456, 0.406],
+                                                                     [0.229, 0.224, 0.225])])
 
-    # return parsed argument collection
-    return parser.parse_args()
+train_data = datasets.ImageFolder(train_dir, transform=train_transforms)
+validation_data = datasets.ImageFolder(valid_dir, transform=validation_transforms)
+test_data = datasets.ImageFolder(test_dir ,transform = test_transforms)
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=64, shuffle=True)
+vloader = torch.utils.data.DataLoader(validation_data, batch_size =32,shuffle = True)
+testloader = torch.utils.data.DataLoader(test_data, batch_size = 20, shuffle = True)
 
 
-#Define function to predict top K classes
-def predict(image_path, model, device, topk=5):
-    ''' Predict the class (or classes) of an image using a trained deep learning model.
-    
-    Parameters:
-     image_path - string, path to input image
-     model - torch.nn.Module, model to use for prediction
-     device - string, the device on which the prediction should be done
-     topk - integer, return top K most likely classes
-    Returns:
-     probs - numpy.ndarray, probabilities of the top k most likely classes
-     classes - numpy.ndarray, top K most likely classes
-    '''
-    
-    # DONE: Implement the code to predict the class from an image file
-    image = Image.open(image_path).convert('RGB')
-    image = process_image(image)
-    image = torch.from_numpy(image).unsqueeze_(0).float()
-    
-    model = model.to(device)
-    image = image.to(device)
-    model.eval()
-    
-    # Calculate class probabilities 
-    with torch.no_grad():
-        outputs = model.forward(image)
-    
-    # Get topk probabilities and classes
-    probs, class_idxs = outputs.topk(topk)
-    
-    probs, class_idxs = probs.to('cpu'), class_idxs.to('cpu')
-    probs = probs.exp().data.numpy()[0]
-    class_idxs = class_idxs.data.numpy()[0]
-    
-    # Convert from indices to the actual class labels
-    classes = np.array([model.idx_to_class[idx] for idx in class_idxs])
-    
-    return probs, classes
+
+utils.load_checkpoint(path)
 
 
-# Call to main function to run the program
-if __name__ == "__main__":
-    main()
+with open('cat_to_name.json', 'r') as json_file:
+    cat_to_name = json.load(json_file)
+
+
+probabilities = utils.predict(path_image, model, number_of_outputs, power)
+
+
+labels = [cat_to_name[str(index + 1)] for index in np.array(probabilities[1][0])]
+probability = np.array(probabilities[0][0])
+
+
+i=0
+while i < number_of_outputs:
+    print("{} with a probability of {}".format(labels[i], probability[i]))
+    i += 1
+
+print("Completed Execution")
